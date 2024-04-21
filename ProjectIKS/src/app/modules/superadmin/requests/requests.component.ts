8dcbd70f38f0cb4d618e2d033975b1bf8b4af22c
Luka Popović <popovicluka65@gmail.com>
2024-04-21 15:10:48 +0200

    add interceptor for certificate

--- ProjectIKS/src/app/modules/superadmin/requests/requests.component.ts
+++ ProjectIKS/src/app/modules/superadmin/requests/requests.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Accommodation} from "../../../models/accommodation";
+import {AccommodationService} from "../../accommodations/service/accommodation.service";
+import {LoginService} from "../../auth/login/service/login.service";
+import {Router} from "@angular/router";
+import {UserServiceService} from "../../unregistered-user/signup/user-service.service";
+import {SuperAdminService} from "../service/superadmin.service";
+import {RequestDTO} from "../../../models/request";
 
 @Component({
   selector: 'app-requests',
   templateUrl: './requests.component.html',
   styleUrls: ['./requests.component.css']
 })
-export class RequestsComponent {
+export class RequestsComponent implements OnInit {
+
+  requests: RequestDTO[] = [];
+
+  constructor(private service: SuperAdminService ,public loginService:LoginService, private router : Router, private userService: UserServiceService) {}
+
+    ngOnInit(): void {
+      this.service.getAll().subscribe({
+        next: (data: RequestDTO[]) => {
+          this.requests = data
+          console.log("REQUESTOVI")
+          console.log(this.requests)
+        }
+      })
+    }
 
 }
--- ProjectIKS/src/app/modules/superadmin/service/superadmin.service.ts
+++ ProjectIKS/src/app/modules/superadmin/service/superadmin.service.ts
@@ -3,6 +3,9 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environment/environment";
 import {AuthResponse} from "../../../models/auth-response";
 import {CertificateRequest} from "../../../models/certificateRequest";
+import {BehaviorSubject, Observable} from "rxjs";
+import {Accommodation} from "../../../models/accommodation";
+import {RequestDTO} from "../../../models/request";
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +16,22 @@ export class SuperAdminService {
     'Content-Type': 'application/json',
     skip: 'true',
   });
-  constructor(private http: HttpClient) {
+  public requestsSubject = new BehaviorSubject<any[]>([]);
+
+  requests$ = this.requestsSubject.asObservable();
+   constructor(private http: HttpClient) {
   }
-  //gadjamo bekend sa PKI
+
+  getAll(): Observable<RequestDTO[]> {
+    return this.http.get<RequestDTO[]>(environment.apiPKI+ '/certificates/get-requests')
+  }
+
   requestCertificate(request:CertificateRequest):any {
     console.log(request)
     return this.http.post<any>(environment.apiPKI+'/certificates', request);
+  }
+  addCertificate(request: any):any {
+     console.log("UDJE FUNKCIJA")
+    return this.http.post(environment.apiPKI + '/certificates', request)
   }
 }
